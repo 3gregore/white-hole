d1ce63c2b71aa7cd948defb058501e5b6c39e802
Paul Smith <dante@DESKTOP-GI5NL18.egregore.lan>
2023-04-19 22:01:23 -0500

    Fixed incorrect sed command.

--- build-whitelist.sh
+++ build-whitelist.sh
@@ -4,20 +4,23 @@ echo "" > Pihole-Whitelist.sh
 
 #Pi-Hole Community Whitelist
 wget https://raw.githubusercontent.com/3gregore/white-hole/main/common_urls.txt
-#Anudeep - Whitelist
+
+#anudeepND Whitelist
 wget https://raw.githubusercontent.com/anudeepND/whitelist/master/domains/whitelist.txt
-#Anudeep - Optional
+
+#anudeepND Optional List
 wget https://raw.githubusercontent.com/anudeepND/whitelist/master/domains/optional-list.txt
-#Anudeep - Referral Sites
+
+#anudeepND Referral Sites
 wget https://raw.githubusercontent.com/anudeepND/whitelist/master/domains/referral-sites.txt
 
 for f in $(ls)
 do
-	if [[ "$f" == *.txt ]]
-	then
-		sed '/^\s*$/d; /^#/!s/.*/&\nsleep 0.1/' $f >> Pihole-Whitelist.sh
-		rm $f
-	fi
+    if [[ "$f" == *.txt ]]
+    then
+        sed '/^\s*$/d; /^#/!s/.*/pihole -w &\nsleep 0.1/' $f >> Pihole-Whitelist.sh
+        rm $f
+    fi
 done
 
 printf "\npihole updateGravity" >> Pihole-Whitelist.sh
